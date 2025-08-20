c87d09111454fbbcba4a1f6be13b6b040d235cc9
Raju S Baradur <125146841+RAJUS248@users.noreply.github.com>
2025-08-20 18:58:29 +0530

    Add files via upload

---
+++ 02_variables.py
@@ -0,0 +1,26 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+i1 = int(input())
+print( i + i1 )
+
+d1 = float(input())
+print(d + d1)
+
+s1 = input()
+print(f"{s}{s1}")
+
+"""
+Sample Input
+
+12
+4.0
+is the best place to learn and practice coding!
+Sample Output
+
+16
+8.0
+HackerRank is the best place to learn and practice coding!
+
+"""
